8debb30ab787cf16c7fc9f67e1ee5b522552bb68
Alejandro Rios U <ariosu.everis@bancochile.cl>
2023-11-23 14:41:08 -0300

    for e if en angular

--- src/app/directiva/directiva.component.ts
+++ src/app/directiva/directiva.component.ts
@@ -8,6 +8,12 @@ import { Component, OnInit } from '@angular/core';
 export class DirectivaComponent {
 
   listaCurso: string[] = ['TypeScrip','JavaScrip','Java SE','C#','php']
+  habilitar: boolean = true;
+
   constructor() { }
+
+  setHabilitar(): void{
+    this.habilitar = (this.habilitar == true)? false: true;
+  }
 
 }
