727ee9906ef1a3f5a1a2754d01af292061eb472d
Alejandro Rios U <ariosu.everis@bancochile.cl>
2023-11-23 15:10:48 -0300

    Listados

---
+++ src/app/clientes/clientes.component.ts
@@ -0,0 +1,28 @@
+import { Component, OnInit } from '@angular/core';
+import{Cliente} from './cliente'
+
+@Component({
+  selector: 'app-clientes',
+  templateUrl: './clientes.component.html'
+})
+export class ClientesComponent implements OnInit {
+
+  clientes:Cliente[]=[
+    {id:1,nombre:"fernando",apellido:'rios',emeal:'correoprueba@emeal.com',createAt:'2023-12-01'},
+    {id:2,nombre:"fernando1",apellido:'rios1',emeal:'1correoprueba@emeal.com',createAt:'2023-12-02'},
+    {id:3,nombre:"fernando2",apellido:'rios2',emeal:'2correoprueba@emeal.com',createAt:'2023-12-03'},
+    {id:4,nombre:"fernando3",apellido:'rios3',emeal:'3correoprueba@emeal.com',createAt:'2023-12-04'},
+    {id:5,nombre:"fernando4",apellido:'rios4',emeal:'4correoprueba@emeal.com',createAt:'2023-12-05'},
+    {id:6,nombre:"fernando5",apellido:'rios5',emeal:'5correoprueba@emeal.com',createAt:'2023-12-06'},
+    {id:7,nombre:"fernando6",apellido:'rios6',emeal:'6correoprueba@emeal.com',createAt:'2023-12-07'},
+    {id:8,nombre:"fernando7",apellido:'rios7',emeal:'7correoprueba@emeal.com',createAt:'2023-12-08'},
+    {id:9,nombre:"fernando8",apellido:'rios8',emeal:'8correoprueba@emeal.com',createAt:'2023-12-09'},
+    {id:10,nombre:"fernand9o9",apellido:'ri9os',emeal:'9correoprueba@emeal.com',createAt:'2023-12-11'}
+  ]
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
